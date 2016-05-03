3519e0e9aeb0fb95da6734fbbe821c719889db86
Melvin Wong <melvin.wong@cantire.com>
2016-05-03 16:26:31 -0500

    add missing attributes to sprint report resource

--- lib/jira_agile_api/resources/sprint_report.rb
+++ lib/jira_agile_api/resources/sprint_report.rb
@@ -4,12 +4,18 @@ module JiraAgileApi
       include Virtus.model
 
       attribute :completedIssues, Array[JiraAgileApi::Resources::Issue]
-      attribute :incompletedIssues, Array[JiraAgileApi::Resources::Issue]
+      attribute :issuesNotCompletedInCurrentSprint, Array[JiraAgileApi::Resources::Issue]
       attribute :puntedIssues, Array[JiraAgileApi::Resources::Issue]
+      attribute :issuesCompletedInAnotherSprint, Array[JiraAgileApi::Resources::Issue]
+      attribute :completedIssuesInitialEstimateSum, JiraAgileApi::Resources::EstimateSum
       attribute :completedIssuesEstimateSum, JiraAgileApi::Resources::EstimateSum
-      attribute :incompletedIssuesEstimateSum, JiraAgileApi::Resources::EstimateSum
+      attribute :issuesNotCompletedInitialEstimateSum, JiraAgileApi::Resources::EstimateSum
+      attribute :issuesNotCompletedEstimateSum, JiraAgileApi::Resources::EstimateSum
       attribute :allIssuesEstimateSum, JiraAgileApi::Resources::EstimateSum
+      attribute :puntedIssuesInitialEstimateSum, JiraAgileApi::Resources::EstimateSum
       attribute :puntedIssuesEstimateSum, JiraAgileApi::Resources::EstimateSum
+      attribute :issuesCompletedInAnotherSprintInitialEstimateSum, JiraAgileApi::Resources::EstimateSum
+      attribute :issuesCompletedInAnotherSprintEstimateSum, JiraAgileApi::Resources::EstimateSum
       attribute :issueKeysAddedDuringSprint, Hash[String => Boolean]
     end
   end
